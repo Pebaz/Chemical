b93ea348e102c3b5c41fa1d15fd71ecc1c966a23
Pebaz <pebazium@gmail.com>
2020-05-08 14:24:02 -0400

    added more unit tests

--- tests/test_chemical.py
+++ tests/test_chemical.py
@@ -4,3 +4,17 @@ from chemical import it
 def test_it():
     assert list(it(range(3))) == [0, 1, 2]
     assert list(it('abc')) == ['a', 'b', 'c']
+
+
+def test_collect():
+    assert it(range(3)).collect() == [0, 1, 2]
+    assert it(range(3)).collect(tuple) == (0, 1, 2)
+    assert it(range(3)).collect(set) == {0, 1, 2}
+    class MyList(list): ...
+    assert it(range(3)).collect(MyList) == MyList((0, 1, 2))
+
+
+def test_skip():
+    assert it(range(3)).skip(1).collect() == [1, 2]
+    assert it(range(100)).skip(10).take(10).collect() == [*range(10, 20)]
+    assert it(range(10)).skip(1).skip(1).collect() == [*range(2, 10)]
