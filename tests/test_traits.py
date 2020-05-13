bf0caa9c69db065ba720ce9dd5a9fe4de45cc487
Pebaz <pebazium@gmail.com>
2020-05-13 16:45:03 -0400

    added more trait tests

--- tests/test_traits.py
+++ tests/test_traits.py
@@ -1,8 +1,32 @@
 from chemical import it, trait
 
+
+@trait
+class Goodbye(it):
+    def __init__(self, items, adder):
+        it.__init__(self, items)
+        self.adder = adder
+
+    def __next__(self):
+        return next(self.items) + self.adder
+
+
+def test_goodbye():
+    assert it('abc').goodbye('!').collect() == ['a!', 'b!', 'c!']
+
+
 @trait
 def blubber(self):
     return 42 + self.count()
 
-def test_traits():
+
+def test_blubber():
     assert it('abc').blubber() == 45
+
+
+trait('hello')(lambda self: list(self))
+
+
+def test_hello():
+    assert it('abc').hello() == ['a', 'b', 'c']
+
