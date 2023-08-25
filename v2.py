956786edefbe3f97e4dc545a0cdde5d8b62ac8f5
Pebaz <pebazium@gmail.com>
2023-08-24 23:33:42 -0700

    New approach wont scale, either use stack machine or recurse

--- v2.py
+++ v2.py
@@ -1,6 +1,6 @@
 class Ag: ...
 class It:
-    iterators = 'next', 'skip', 'take', 'step_by', 'count'
+    iterators = 'next', 'skip', 'take', 'step_by', 'rev'
     aggregators = 'count',
     def __init__(self, *args, **kwargs):
         self.input = args, kwargs
@@ -17,60 +17,51 @@ class It:
     def __call__(self, *args, **kwargs):
         if self.instructions and isinstance(self.instructions[-1], str):
             command = self.instructions.pop()
-            if command in self.aggregators:
-                return self.aggregate(command, *args, **kwargs)
-            else:
+            if command not in self.aggregators:
                 self.instructions.append((command, args, kwargs))
+            else:
+                return self.aggregate(command, *args, **kwargs)
         return self
+    # Instructions take arguments as input and incrementally produce output
     def __iter__(self):
         # In order for pausing and resuming to work, these have to be fields
-        collection = self.input[0][0]
-        index = 0
+        input = self.input[0][0]
+        forward, backward = iter(input), reversed(input)
+        reversing = False
+        consume = lambda: (next(forward), next(backward))[reversing]
+        index = 0  # TODO(pbz): Not strictly needed but may be useful
         step = 1
-        print(self.instructions)
         for instruction in self.instructions:
-            print(instruction)
             match instruction:
-                case ['next', args, kwargs]:
-                    yield collection[index]
+                case ['skip', [amount], _]:
                     index += amount
-                case ['skip', [amount], kwargs]:
-                    index += amount
-                case ['take', [amount], kwargs]:
                     for _ in range(amount):
-                        yield collection[index]
+                        consume()
+                case ['take', [amount], _]:
+                    for _ in range(amount):
                         index += step
-                case ['step_by', [amount], kwargs]:
+                        yield consume()
+                case ['step_by', [amount], _]:
                     step = amount
-                case ['count', *_]:
-                    count = len(collection[index:])
-                    print('count:', count)
-                    yield count
+                case ['rev', *_]:
+                    reversing = not reversing
                 case _:
                     raise Exception("Unreachable")
-            # if index == len(collection):
-            #     return
+        try:
+            while True:
+                yield consume()
+                index += step
+                for _ in range(step - 1):
+                    consume()
+        except StopIteration:
+            ...
+    # Aggregators take self as input and produces a singular output
     def aggregate(self, command, *args, **kwargs):
-        match command:
-            case 'count':
-                count = 0
-                for _ in self:
-                    count += 1
-                return count
-            case _:
-                pass
-
-    # TODO(pbz): If the instructions are defined on the class, do user's care?
-    # def skip(self, amount):
-    #     # The utility of this is to validate parameters at first call when
-    #     # building the iterator chain and then when actually iterating, perform
-    #     # the instruction
-    #     if not self.iterating:
-    #         return
-
-# NOTE(pbz): For now, API design with user-defined "trait" functions is not a
-# NOTE(pbz): goal. Focus on fixing the deeply recursive nature by making it a
-# NOTE(pbz): big state machine.
+        if command == 'count':
+            count = 0
+            for _ in self:
+                count += 1
+            return count
 
 # Syntax example: it([1, 2, 3])( ??? )
 # Exception.with_traceback()
@@ -78,7 +69,5 @@ class It:
 
 it = It([1, 2, 3, 4, 5])
 it = it.skip(1).step_by(2).take(2)
-# print(it)
-# print(list(it))
-print('Count:', it.count())
-# print(it.next())
+print(it.count())
+print(list(It([1, 2, 3, 4, 5]).rev().step_by(2).take(2)))
